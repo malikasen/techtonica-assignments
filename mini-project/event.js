506d7d710efd8ed5e0919087823a0f23d55931cf
Malika <malikasenova@gmail.com>
2021-08-02 21:42:23 -0700

    Solve mini-project-OOP

--- mini-project/event.js
+++ mini-project/event.js
@@ -5,6 +5,30 @@ class Event {
     this.description = description;
     this.availableTickets = [];
   }
+  addAvailableTickets(type, price) {
+    this.availableTickets.push(new TicketType(type, price))
+  }
+  allTickets() {
+    let str = 'All tickets: ';
+    for (let i = 0; i < this.availableTickets.length; i++) {
+      str += i + 1 + '. ' + this.availableTickets[i]['name'] + ' ($' + this.availableTickets[i]['price'] + ') ';
+    }
+    return str;
+  }
+  searchTickets(lowerPrice, upperPrice) {
+    let str = 'Eligible tickets: ';
+    let count = 0;
+    for (let i = 0; i < this.availableTickets.length; i++) {
+      if(this.availableTickets[i]['price'] >= lowerPrice && this.availableTickets[i]['price'] <= upperPrice) {
+        count ++;
+        str += count + '. ' + this.availableTickets[i]['name'] + ' ($' + this.availableTickets[i]['price'] + ') ';
+      }
+    }
+    if (str === 'Eligible tickets: ') {
+      str = 'No tickets available.'
+    }
+    return str;
+  }
 }
 
 // The below statement creates an object.
@@ -33,7 +57,7 @@ console.log(eventArray);
 $(document).ready(function () {
   let html = '';
   $.each(eventArray, function (index, item) {
-    html += `<li>${item.name} - ${item.description}</li>`;
+    html += `<li>${item.name} - ${item.description} - ${item.searchTickets(0, 100)}</li>`;
   });
   // insert final html into #event...
   $('#event').html(html);
@@ -46,3 +70,19 @@ class TicketType {
     this.price = price;
   }
 }
+
+// Create a ticket type for the event
+eventObj1.addAvailableTickets("human", 299);
+eventObj1.addAvailableTickets("vampire", 99);
+
+eventObj2.addAvailableTickets("General Admission", 25)
+eventObj2.addAvailableTickets("Floor Seating", 80)
+
+eventObj3.addAvailableTickets("Orchestra", 300)
+eventObj3.addAvailableTickets("Mezzanine", 200)
+eventObj3.addAvailableTickets("Balcony", 100)
+
+console.log(eventObj3);
+console.log(eventObj3.allTickets());
+console.log(eventObj3.searchTickets(100, 250));
+
