a1e655e29fee295d95693d86c950f6249b42e96b
Malika <malikasenova@gmail.com>
2021-08-03 21:18:13 -0700

    Implement generateMessage function using DOM

--- game-project/script.js
+++ game-project/script.js
@@ -2,16 +2,41 @@ function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-let lower = document.getElementById("minNum").value;
-let lower = document.getElementById("maxNum").value;
+function generateMessage() {
+  // debugger
+  if (inputsEntered()) {
+    label1.textContent = 'The number is generated!'
+    return getRandomNumber(getMin, getMax);
+  }
+}
+
+function inputsEntered() {
+  // debugger
+  // console.log(getMin + ' ' + getMax)
+  if (getMin() != '' && getMax() != '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getMin() {
+  return document.getElementById('minNum').value;
+}
+function getMax() {
+  return document.getElementById('maxNum').value;
+}
+var label1 = document.querySelector('#missingInput');
+var buttonGenerate = document.querySelector('#generate');
+buttonGenerate.addEventListener('click', generateMessage);
 
 
 
 let str;
 
-if (getRandomNumber(lower, upper) > guess) {
+if (getRandomNumber(getMin(), getMax()) > guess) {
   str = 'The number is higher. Try again.';
-} else if (getRandomNumber(lower, upper) < guess) {
+} else if (getRandomNumber(getMin(), getMax()) < guess) {
   str = 'The number is lower. Try again.';
 } else {
   str = 'You guessed it right!!!'
