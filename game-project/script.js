0dca1c96e5d48d3e20f83b32de1a4263bc471e9d
Malika <malikasenova@gmail.com>
2021-08-03 22:45:59 -0700

    Fix guess and randomNumber matching issue & add past guesses

--- game-project/script.js
+++ game-project/script.js
@@ -2,9 +2,12 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+
+
 function generateMessage() {
   if (inputsEntered()) {
-    label1.textContent = `The number is generated! It is between ${getMin()} and ${getMax()}.`
+    randomNumber = getRandomNumber(getMin(), getMax());
+    label1.textContent = `The number is generated! It is between ${getMin()} and ${getMax()}. It is ${randomNumber}.`
     return getRandomNumber(getMin(), getMax());
   }
 }
@@ -24,21 +27,25 @@ function getMin() {
 function getMax() {
   return Number(document.getElementById('maxNum').value);
 }
-
+let pastGuesses = [];
 function generateResult() {
   let guess = Number(document.getElementById('guess').value);
   if (guess != '') {
     let str;
-    if (getRandomNumber(getMin(), getMax()) > guess) {
+    pastGuesses.push(guess);
+    if (randomNumber > guess) {
       str = 'The number is higher. Try again.';
-    } else if (getRandomNumber(getMin(), getMax()) < guess) {
+    } else if (randomNumber < guess) {
       str = 'The number is lower. Try again.';
     } else {
       str = 'You guessed it right!!!'
     }
     label2.textContent = str;
+    label3.textContent = pastGuesses;
   }
 }
+
+let randomNumber;
 
 var label1 = document.querySelector('#missingInput');
 var buttonGenerate = document.querySelector('#generate');
@@ -46,5 +53,5 @@ buttonGenerate.addEventListener('click', generateMessage);
 var label2 = document.querySelector('#result');
 var buttonTry = document.querySelector('#try');
 buttonTry.addEventListener('click', generateResult);
-
+var label3 = document.querySelector('#guesses');
 
