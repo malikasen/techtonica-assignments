4587c4d4a8a7bcae5c5649f963383304d13ac26d
Malika <malikasenova@gmail.com>
2021-08-04 10:01:05 -0700

    Add stylesheet

--- game-project/script.js
+++ game-project/script.js
@@ -39,6 +39,7 @@ function generateResult() {
       str = 'The number is lower. Try again.';
     } else {
       str = 'You guessed it right!!!'
+      // fireworks.start();
     }
     label2.textContent = str;
     label3.textContent = pastGuesses;
@@ -54,4 +55,50 @@ var label2 = document.querySelector('#result');
 var buttonTry = document.querySelector('#try');
 buttonTry.addEventListener('click', generateResult);
 var label3 = document.querySelector('#guesses');
+
+// https://www.pinterest.com/pin/623678248371870067/
+
+//add fireworks
+// const container = document.querySelector('..fireworks-example')
+// const fireworks = new Fireworks(container, {
+//       rocketsPoint: 50,
+//       hue: { min: 0, max: 360 },
+//       delay: { min: 15, max: 30 },
+//       speed: 2,
+//       acceleration: 1.05,
+//       friction: 0.95,
+//       gravity: 1.5,
+//       particles: 50,
+//       trace: 3,
+//       explosion: 5,
+//       autoresize: true,
+//       brightness: { 
+//         min: 50, 
+//         max: 80,
+//         decay: { min: 0.015, max: 0.03 }
+//       }
+//       mouse: { 
+//         click: false, 
+//         move: false, 
+//         max: 3 
+//       }
+//       boundaries: { 
+//         x: 50, 
+//         y: 50, 
+//         width: container.clientWidth, 
+//         height: container.clientHeight 
+//       }
+//       sound: {
+//         enable: true,
+//         files: [
+//           'explosion0.mp3',
+//           'explosion1.mp3',
+//           'explosion2.mp3'
+//         ],
+//         volume: { min: 1, max: 2 },
+//       }
+// });
+
+
+
 
