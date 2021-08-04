ad1c2c9c1ce96851370312e5db05601e995252c4
Malika <malikasenova@gmail.com>
2021-08-03 22:01:46 -0700

    Implement generateResult function

--- game-project/script.js
+++ game-project/script.js
@@ -1,17 +1,15 @@
 function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function generateMessage() {
-  // debugger
   if (inputsEntered()) {
     label1.textContent = 'The number is generated!'
-    return getRandomNumber(getMin, getMax);
+    return getRandomNumber(getMin(), getMax());
   }
 }
 
 function inputsEntered() {
-  // debugger
   // console.log(getMin + ' ' + getMax)
   if (getMin() != '' && getMax() != '') {
     return true;
@@ -21,23 +19,32 @@ function inputsEntered() {
 }
 
 function getMin() {
-  return document.getElementById('minNum').value;
+  return Number(document.getElementById('minNum').value);
 }
 function getMax() {
-  return document.getElementById('maxNum').value;
+  return Number(document.getElementById('maxNum').value);
 }
+
+function generateResult() {
+  let guess = Number(document.getElementById('guess').value);
+  if (guess != '') {
+    let str;
+    if (getRandomNumber(getMin(), getMax()) > guess) {
+      str = 'The number is higher. Try again.';
+    } else if (getRandomNumber(getMin(), getMax()) < guess) {
+      str = 'The number is lower. Try again.';
+    } else {
+      str = 'You guessed it right!!!'
+    }
+    label2.textContent = str;
+  }
+}
+
 var label1 = document.querySelector('#missingInput');
 var buttonGenerate = document.querySelector('#generate');
 buttonGenerate.addEventListener('click', generateMessage);
+var label2 = document.querySelector('#result');
+var buttonTry = document.querySelector('#try');
+buttonTry.addEventListener('click', generateResult);
 
 
-
-let str;
-
-if (getRandomNumber(getMin(), getMax()) > guess) {
-  str = 'The number is higher. Try again.';
-} else if (getRandomNumber(getMin(), getMax()) < guess) {
-  str = 'The number is lower. Try again.';
-} else {
-  str = 'You guessed it right!!!'
-}
