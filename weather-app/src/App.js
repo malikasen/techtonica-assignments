516789c0f5176cf8652563dbf507911e3680b200
Malika <malikasenova@gmail.com>
2021-08-26 22:24:34 -0700

    Add temp, description, humidity, and wind wording

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,24 +3,46 @@ import './App.css';
 // import moment from 'moment';
 
 function ForecastDay({forecast}) {
+  // const [dayOfWeek, setDayOfWeek] = useState();
+  // const getDayOfWeek = () => {
+  //   const date = new Date(forecast.dt*1000);
+  //   switch(date.getDay()) {
+  //     case 0: setDayOfWeek("Sunday")
+  //     break;
+  //     case 1: setDayOfWeek("Monday")
+  //     break;
+  //     case 2: setDayOfWeek("Tuesday")
+  //     break;
+  //     case 3: setDayOfWeek("Wednesday")
+  //     break;
+  //     case 4: setDayOfWeek("Thursday")
+  //     break;
+  //     case 5: setDayOfWeek("Friday")
+  //     break;
+  //     case 6: setDayOfWeek("Saturday")
+  //     break;
+  //   }
+  // }
+  // getDayOfWeek();
   return (
     <div className='forecastDay'>
-      {/* <h3 id='dayOfWeek'></h3> */}
+      {/* <h3 id='dayOfWeek'>{dayOfWeek}</h3> */}
       <h3 className='date'>{forecast.dt_txt}</h3>
       <i className='weatherIcon'></i>
       <h4 className='temp'>Temp {forecast.main.temp} F</h4>
-      <h4 className='description'>{forecast.weather.description}</h4>
-      <h4 className='humidity'>{forecast.main.humidity}</h4>
-      <h4 className='wind'>{forecast.wind.speed}</h4>
+      <h4 className='description'>{forecast.weather[0].description}</h4>
+      <h4 className='humidity'>Humidity: {forecast.main.humidity}</h4>
+      <h4 className='wind'>Wind: speed {forecast.wind.speed}, deg {forecast.wind.deg}</h4>
     </div>
   )
 }
 
 function Forecast({city}) {
-  const [forecasts, setForecasts] =useState([]);
+  const [forecasts, setForecasts] = useState([]);
   const fetchData = () => {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=799fecc631c47adc89b3f3f3e590285b")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=3d1dfd49bd39d14e53c1764e6af8b0cc")
     // 5day/3 hour "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=3d1dfd49bd39d14e53c1764e6af8b0cc" 
+    // 799fecc631c47adc89b3f3f3e590285b
     .then((response) => response.json())
     .then(function(json) {
       console.log(json);
@@ -49,7 +71,6 @@ function Forecast({city}) {
 
 function Form({setCity}) {
   function onClick() {
-    console.log('hello');
     const cityElement = document.getElementById('city');
     const city = cityElement.value;
     setCity(city);
