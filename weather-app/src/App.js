5b17037df0f5c9956688478ec8c13ccde7848c2f
Malika <malikasenova@gmail.com>
2021-08-30 17:05:35 -0700

    Add icons and somce css

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,34 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import './App.css';
 // import moment from 'moment';
 
 function ForecastDay({forecast}) {
-  // const [dayOfWeek, setDayOfWeek] = useState();
-  // const getDayOfWeek = () => {
-  //   const date = new Date(forecast.dt*1000);
-  //   switch(date.getDay()) {
-  //     case 0: setDayOfWeek("Sunday")
-  //     break;
-  //     case 1: setDayOfWeek("Monday")
-  //     break;
-  //     case 2: setDayOfWeek("Tuesday")
-  //     break;
-  //     case 3: setDayOfWeek("Wednesday")
-  //     break;
-  //     case 4: setDayOfWeek("Thursday")
-  //     break;
-  //     case 5: setDayOfWeek("Friday")
-  //     break;
-  //     case 6: setDayOfWeek("Saturday")
-  //     break;
-  //   }
-  // }
-  // getDayOfWeek();
+
+  const getDayOfWeek = () => {
+    const date = new Date(forecast.dt*1000);
+    switch(date.getDay()) {
+      case 0: return "Sunday"
+      case 1: return "Monday"
+      case 2: return "Tuesday"
+      case 3: return "Wednesday"
+      case 4: return "Thursday"
+      case 5: return "Friday"
+      case 6: return "Saturday"
+    }
+  }
+  const iconString = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`
+  console.log(iconString);
   return (
-    <div className='forecastDay'>
-      {/* <h3 id='dayOfWeek'>{dayOfWeek}</h3> */}
-      <h3 className='date'>{forecast.dt_txt}</h3>
-      <span className='weatherIcon' src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`} alt='Icon weather should be here'></span>
+    <div className='forecastDay' class='col ForecastCard'>
+      <h3 id='dayOfWeek'>{getDayOfWeek()}</h3>
+      <h3 className='date'>{forecast.dt_txt.substring(0,11)}</h3>
+      <img className='weatherIcon' src={iconString} alt='Icon weather should be here'></img>
       <h4 className='temp'>Temp {forecast.main.temp} F</h4>
       <h4 className='description'>{forecast.weather[0].description}</h4>
       <h4 className='humidity'>Humidity: {forecast.main.humidity}</h4>
@@ -39,48 +33,49 @@ function ForecastDay({forecast}) {
 
 function Forecast({city}) {
   const [forecasts, setForecasts] = useState([]);
-  const fetchData = () => {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=dd404e88f1240ad89413f450ca007d1d")
-    // 5day/3 hour "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=3d1dfd49bd39d14e53c1764e6af8b0cc" 
-    // 799fecc631c47adc89b3f3f3e590285b
-    // c2a342ca948b9c3c397372c123d2a67a
-    .then((response) => response.json())
-    .then(function(json) {
-      console.log(json);
-      const forecastIndexes = [3,11,19,27,35];
-      if (json.list !== undefined) {
-        const forecastsAtNoon = json.list.filter((forecast,index) => {
-          return forecastIndexes.includes(index);
-        });
-        setForecasts(forecastsAtNoon);
-      }
-    })
-  }
-  useEffect(() => {
+  useMemo(() => {
     if (city !== undefined) {
-      fetchData()
+      fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=dd404e88f1240ad89413f450ca007d1d")
+      // 5day/3 hour "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=3d1dfd49bd39d14e53c1764e6af8b0cc" 
+      // 799fecc631c47adc89b3f3f3e590285b
+      // c2a342ca948b9c3c397372c123d2a67a
+      .then((response) => response.json())
+      .then(function(json) {
+        console.log(json);
+        const forecastIndexes = [3,11,19,27,35];
+        if (json.list !== undefined) {
+          const forecastsAtNoon = json.list.filter((forecast,index) => {
+            return forecastIndexes.includes(index);
+          });
+          setForecasts(forecastsAtNoon);
+        }
+      })
     }
-  })
+  }, [city])
+  
   return (
-    <div className='forecastList'>
-      {forecasts.map((forecast, index) => {
-        return <ForecastDay forecast={forecast} key={index}/>
-      })}
+    <div className='forecastList' class='container'>
+      <div class='row'>
+        {forecasts.map((forecast, index) => {
+          return <ForecastDay forecast={forecast} key={index}/>
+        })}
+      </div>
     </div>
   )
 }
 
 function Form({setCity}) {
-  function onClick() {
+  function onClick(event) {
+    event.preventDefault();
     const cityElement = document.getElementById('city');
     const city = cityElement.value;
     setCity(city);
   }
   return (
     <div>
-      <form>
+      <form onSubmit={onClick}>
         <input type='text' name='city' id='city' placeholder='Lookup Location'></input>
-        <input type='button' onClick={onClick} id='submit' value='submit'></input>
+        <input type='submit' id='submit' value='submit'></input>
       </form>
     </div>
   )
