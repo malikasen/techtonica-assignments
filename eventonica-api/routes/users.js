bf367930d0f44ae92b28d598661ea54a851377ae
Malika <malikasenova@gmail.com>
2021-09-09 15:11:17 -0700

    Create basic DB tables and install pg-promise

--- eventonica-api/routes/users.js
+++ eventonica-api/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const marlin = { name: "Marlin", email: "marlin@gmail.com", id:"1" };
-const nemo = { name: "Nemo", email: "nemo@gmail.com", id: "2" };
-const dory = { name: "Dory", email: "dory@gmail.com" , id: "3"};
+const marlin = { name: "Marlin", email: "marlin@gmail.com", userId:"1" };
+const nemo = { name: "Nemo", email: "nemo@gmail.com", userId: "2" };
+const dory = { name: "Dory", email: "dory@gmail.com" , userId: "3"};
 const users =[marlin, nemo, dory];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
