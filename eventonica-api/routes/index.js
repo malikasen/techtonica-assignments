34466382230656cc012cdcbd8e18903289bbaf1c
Malika <malikasenova@gmail.com>
2021-09-13 17:53:36 -0700

    Clean up backend users and index files

--- eventonica-api/routes/index.js
+++ eventonica-api/routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 var pgp = require('pg-promise')(/* options */)
 var db = pgp('postgres://localhost:5432/eventonica')
-// function addUser(data) {
-//   // note: this returns a Promise
-//   console.log(data);
-//   return db.one('INSERT INTO users (name, email) values (\$1, \$2) RETURNING id, name, email', [data.name, data.email]);
-// }
+
 db.one('SELECT $1 AS value', 123)
   .then(function (data) {
     console.log('DATA:', data.value)
@@ -19,12 +15,7 @@ db.one('SELECT $1 AS value', 123)
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Our express app is working properly' });
-  // db.any('SELECT * FROM users ')
-  // .then(data => console.log(data))
 });
 
-// router.post('/users', (req, res) => {
-//   addUser(req.body).then(() => res.sendStatus(204));
-// });
 
 module.exports = router;
--- eventonica-api/routes/users.js
+++ eventonica-api/routes/users.js
@@ -3,18 +3,15 @@ var router = express.Router();
 var pgp = require('pg-promise')(/* options */)
 var db = pgp('postgres://localhost:5432/eventonica');
 
-const marlin = { name: "Marlin", email: "marlin@gmail.com", userId:"1" };
-const nemo = { name: "Nemo", email: "nemo@gmail.com", userId: "2" };
-const dory = { name: "Dory", email: "dory@gmail.com" , userId: "3"};
-const users =[marlin, nemo, dory];
+// const marlin = { name: "Marlin", email: "marlin@gmail.com", userId:"1" };
+// const nemo = { name: "Nemo", email: "nemo@gmail.com", userId: "2" };
+// const dory = { name: "Dory", email: "dory@gmail.com" , userId: "3"};
+// const users =[marlin, nemo, dory];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json(users);
 });
-// router.post('/', function(req, res, next) {
-//   // db.addUser(req.body).then(() => res.sendStatus(204));
-//   return db.one('INSERT INTO users (name) values (\$1) RETURNING id, name', [data.name]);
-// });
+
 function addUser(data) {
   // note: this returns a Promise
   console.log(data);
@@ -23,12 +20,11 @@ function addUser(data) {
 router.post('/users', (req, res) => {
   
 });
+
 router.post('/', function(req, res, next) {
   addUser(req.body).then(async() => {
     const users = await db.any('SELECT * FROM users');
     res.json(users);
   });
-  // console.log(req.body);
-  // console.log(JSON.stringify(req.headers));
 })
 module.exports = router;
